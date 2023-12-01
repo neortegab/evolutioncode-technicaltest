6901ef209f3da470908530891eafb6cec9a3d621
Nelson Ortega <ne.beltran18@gmail.com>
2023-11-30 21:07:15 -0500

    feat tests: TaskModel tests created and tested

--- src/main/java/com/example/evolutioncodetest/task/TaskModel.java
+++ src/main/java/com/example/evolutioncodetest/task/TaskModel.java
@@ -1,4 +1,13 @@
 package com.example.evolutioncodetest.task;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Data
+@NoArgsConstructor
 public class TaskModel {
 }
--- src/test/java/com/example/evolutioncodetest/task/ModelTest.java
+++ src/test/java/com/example/evolutioncodetest/task/ModelTest.java
@@ -1,4 +1,39 @@
 package com.example.evolutioncodetest.task;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class ModelTest {
+    @Test
+    void testModelDefinition(){
+        assertThat(TaskModel.class)
+                .as("Task model should have the property \"id\"")
+                .hasDeclaredFields("id");
+        assertThat(TaskModel.class)
+                .as("Task model should have the property \"description\"")
+                .hasDeclaredFields("description");
+        assertThat(TaskModel.class)
+                .as("Task model should have the property \"status\"")
+                .hasDeclaredFields("status");
+    }
+
+    @Test
+    void testModelTypes() throws NoSuchFieldException {
+
+        assertThat(TaskModel.class.getDeclaredField("id").getType().getTypeName())
+                .as("Task model id should be of type UUID")
+                        .isEqualTo("java.util.UUID");
+
+        assertThat(TaskModel.class.getDeclaredField("description").getType().getTypeName())
+                .as("Task model description should be of type String")
+                .isEqualTo("java.lang.String");
+
+        assertThat(TaskModel.class.getDeclaredField("status").getType().getTypeName())
+                .as("Task model status should be of type boolean")
+                .isEqualTo("boolean");
+    }
 }
