ea968d6067d38544dbb88ce43e8ea087696621e6
neortegab <ne.beltran18@gmail.com>
2023-12-01 17:56:36 -0500

    fix servicetest: removed unnecessary imports

--- src/test/java/com/example/evolutioncodetest/task/ServiceTest.java
+++ src/test/java/com/example/evolutioncodetest/task/ServiceTest.java
@@ -1,10 +1,8 @@
 package com.example.evolutioncodetest.task;
 
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
