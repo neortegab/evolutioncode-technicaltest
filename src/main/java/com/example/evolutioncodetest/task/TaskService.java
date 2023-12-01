e1713b95ef959e58d29d72954fab7f3f09ad5ce3
neortegab <ne.beltran18@gmail.com>
2023-12-01 14:20:02 -0500

    feat TaskService: defined Task Service and basic CRUD operations

--- src/main/java/com/example/evolutioncodetest/task/TaskService.java
+++ src/main/java/com/example/evolutioncodetest/task/TaskService.java
@@ -1,4 +1,42 @@
 package com.example.evolutioncodetest.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class TaskService {
+
+    private final TaskRepository repository;
+
+    @Autowired
+    public TaskService(TaskRepository repository){
+        this.repository = repository;
+    }
+
+    public List<TaskModel> findAll(){
+        return repository.findAll();
+    }
+
+    public TaskModel findById(UUID id){
+        return repository.findById(id).orElseThrow();
+    }
+
+    public List<TaskModel> findByDescription(String description){
+        return repository.findByDescriptionContains(description);
+    }
+
+    public TaskModel createTask(TaskModel task){
+        return repository.save(task);
+    }
+
+    public TaskModel updateTask(TaskModel task){
+        return repository.save(task);
+    }
+
+    public void deleteTaskById(UUID id){
+        repository.deleteById(id);
+    }
 }
