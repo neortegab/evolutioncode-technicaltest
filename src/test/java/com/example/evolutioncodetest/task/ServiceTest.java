0b256e56e95e22f58cdd830e6feae94878884580
neortegab <ne.beltran18@gmail.com>
2023-12-01 17:49:30 -0500

    test service: update test implemented

--- src/test/java/com/example/evolutioncodetest/task/ServiceTest.java
+++ src/test/java/com/example/evolutioncodetest/task/ServiceTest.java
@@ -94,7 +94,20 @@ public class ServiceTest {
 
     @Test
     void testUpdate() {
+        var randomId = UUID.randomUUID();
 
+        Mockito.when(repository.findById(randomId)).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> service.updateTask(new TaskModel(randomId, "desc", false)))
+                .as("Update task should throw a NoSuchElementException if the ID doesn't exists")
+                .isInstanceOf(NoSuchElementException.class);
+
+        var updatedTask = new TaskModel(randomId, "desc", true);
+
+        Mockito.when(repository.findById(randomId)).thenReturn(Optional.of(new TaskModel(randomId, "desc", false)));
+        Mockito.when(repository.save(updatedTask)).thenReturn(updatedTask);
+        assertThat(service.updateTask(updatedTask))
+                .as("Update task should update the task when the task id is found")
+                .isNotNull().isInstanceOf(TaskModel.class).isEqualTo(updatedTask);
     }
 
     @Test
